49ca3b96d01a8af33701ed0a9ab93bebbfa2a4cb
vinneAZTRO <vinnesense@gmail.com>
2021-07-21 04:54:55 -0300

    Criei esse repositório para salvar comandos básicos para ver os dados das variaveis.

---
+++ cod.php
@@ -0,0 +1,15 @@
+<?php 
+
+$nick = "@AZTRO";
+$idade = 21;
+$salario = 4500.99;
+$block = false;
+var_dump($nick);
+echo "<br/>";
+var_dump($idade);
+echo "<br/>";
+var_dump($salario);
+echo "<br/>";
+var_dump($block);
+
+?>
